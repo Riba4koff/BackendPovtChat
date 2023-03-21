0e12565c660a263d7fb82f506d1bee1564087d75
Pavel <riba4koff@mail.ru>
2023-03-21 17:52:56 +0300

    Backend

--- src/main/kotlin/ru/povtchat/database/users_chats/UsersChatsDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.backend.database.users_chats
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class UsersChatsDTO(
-    val id_user: String,
-    val id_chat: String
-)
