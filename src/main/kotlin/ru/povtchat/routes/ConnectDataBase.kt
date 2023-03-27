a3d8d99b23e48035c2bc2633848cd94f7ddbe966
Павел <riba4koff@mail.ru>
2023-03-27 18:30:34 +0300

    Подключение к базе данных

--- src/main/kotlin/ru/povtchat/routes/ConnectDataBase.kt
+++ src/main/kotlin/ru/povtchat/routes/ConnectDataBase.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.sql.Database
 fun Application.connectDataBaseRouting(){
     routing {
         Database.connect(
-            url = "postgresql://postgres:FtHCZKVPTPkAXpZs5FNk@containers-us-west-42.railway.app:7382/railway"/*"jdbc:postgresql://localhost/postgres"*/,
+            url = "postgres:FtHCZKVPTPkAXpZs5FNk@containers-us-west-42.railway.app:7382/railway"/*"jdbc:postgresql://localhost/postgres"*/,
             driver = "org.postgresql.Driver",
             user = "postgres",
             password = "FtHCZKVPTPkAXpZs5FNk"/*"2Q2LYE!DDF3e"*/
